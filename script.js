f826809406d36ea278ead49eb4ca0c091607148f
MemeDigits <130191257+MemeDigits@users.noreply.github.com>
2023-04-07 22:58:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,83 @@
-const board = document.querySelector('.board');
-const cells = document.querySelectorAll('[data-cell]');
-const result = document.querySelector('#result');
-const restart = document.querySelector('#restart');
-const currentPlayerDisplay = document.querySelector('#current-player');
+const startBtn = document.getElementById('start-btn');
+startBtn.addEventListener('click', startGame);
 
+let isGameActive = false;
 let currentPlayer = '69';
-let gameEnd = false;
 
+const gameBoard = document.getElementById('game-board');
 const winningCombinations = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
 ];
 
-startGame();
-
-restart.addEventListener('click', startGame);
+let boardState = ['', '', '', '', '', '', '', '', ''];
 
 function startGame() {
-  gameEnd = false;
-  cells.forEach(cell => {
-    cell.classList.remove('69', '420');
-    cell.removeEventListener('click', handleClick);
-    cell.addEventListener('click', handleClick, { once: true });
-  });
-  setBoardHoverClass();
-  result.textContent = '';
-  currentPlayer = '69';
-  currentPlayerDisplay.textContent = '69';
+	isGameActive = true;
+	currentPlayer = '69';
+	boardState = ['', '', '', '', '', '', '', '', ''];
+	gameBoard.innerHTML = '';
+	renderBoard();
 }
 
-function handleClick(e) {
-  const cell = e.target;
-  const currentClass = currentPlayer === '69' ? '69' : '420';
-  placeMark(cell, currentClass);
-  if (checkWin(currentClass)) {
-    endGame(false);
-  } else if (isDraw()) {
-    endGame(true);
-  } else {
-    swapTurns();
-    setBoardHoverClass
+function renderBoard() {
+	for (let i = 0; i < 9; i++) {
+		const box = document.createElement('div');
+		box.classList.add('box');
+		box.setAttribute('data-index', i);
+		box.addEventListener('click', handleBoxClick);
+		box.innerText = boardState[i] === '' ? '' : boardState[i] === '69' ? '69' : '420';
+		if (boardState[i] === '69') {
+			box.classList.add('player-69');
+		} else if (boardState[i] === '420') {
+			box.classList.add('player-420');
+		}
+		gameBoard.appendChild(box);
+	}
+}
+
+function handleBoxClick(event) {
+	const clickedBox = event.target;
+	const clickedBoxIndex = clickedBox.getAttribute('data-index');
+	if (boardState[clickedBoxIndex] !== '' || !isGameActive) {
+		return;
+	}
+	handlePlayerMove(clickedBox, clickedBoxIndex);
+	handleResult();
+}
+
+function handlePlayerMove(clickedBox, clickedBoxIndex) {
+	boardState[clickedBoxIndex] = currentPlayer;
+	clickedBox.innerText = currentPlayer;
+	if (currentPlayer === '69') {
+		clickedBox.classList.add('player-69');
+		currentPlayer = '420';
+	} else {
+		clickedBox.classList.add('player-420');
+		currentPlayer = '69';
+	}
+}
+
+function handleResult() {
+	for (let i = 0; i < winningCombinations.length; i++) {
+		const [a, b, c] = winningCombinations[i];
+		if (boardState[a] === '' || boardState[b] === '' || boardState[c] === '') {
+			continue;
+		}
+		if (boardState[a] === boardState[b] && boardState[b] === boardState[c]) {
+			isGameActive = false;
+			alert(`Player ${boardState[a]} has won!`);
+			break;
+		}
+	}
+	if (!boardState.includes('') && isGameActive) {
+		isGameActive = false;
+		alert('Tie!');
+	}
+}
