9e059d83017da8094d4baf2e027072b9eeb3473a
MemeDigits <130191257+MemeDigits@users.noreply.github.com>
2023-04-07 22:52:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-const X_CLASS = '69'
-const O_CLASS = '420'
-const WINNING_COMBINATIONS = [
+const board = document.querySelector('.board');
+const cells = document.querySelectorAll('[data-cell]');
+const result = document.querySelector('#result');
+const restart = document.querySelector('#restart');
+const currentPlayerDisplay = document.querySelector('#current-player');
+
+let currentPlayer = '69';
+let gameEnd = false;
+
+const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -9,42 +16,33 @@ const WINNING_COMBINATIONS = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6]
-]
-const cellElements = document.querySelectorAll('[data-cell]')
-const board = document.querySelector('.board')
-const messageText = document.querySelector('.message')
-let oTurn
+];
 
-startGame()
+startGame();
+
+restart.addEventListener('click', startGame);
 
 function startGame() {
-  oTurn = false
-  cellElements.forEach(cell => {
-    cell.classList.remove(X_CLASS)
-    cell.classList.remove(O_CLASS)
-    cell.removeEventListener('click', handleClick)
-    cell.addEventListener('click', handleClick, { once: true })
-  })
-  setBoardHoverClass()
-  messageText.innerText = ''
+  gameEnd = false;
+  cells.forEach(cell => {
+    cell.classList.remove('69', '420');
+    cell.removeEventListener('click', handleClick);
+    cell.addEventListener('click', handleClick, { once: true });
+  });
+  setBoardHoverClass();
+  result.textContent = '';
+  currentPlayer = '69';
+  currentPlayerDisplay.textContent = '69';
 }
 
 function handleClick(e) {
-  const cell = e.target
-  const currentClass = oTurn ? O_CLASS : X_CLASS
-  placeMark(cell, currentClass)
+  const cell = e.target;
+  const currentClass = currentPlayer === '69' ? '69' : '420';
+  placeMark(cell, currentClass);
   if (checkWin(currentClass)) {
-    endGame(false)
+    endGame(false);
   } else if (isDraw()) {
-    endGame(true)
+    endGame(true);
   } else {
-    swapTurns()
-    setBoardHoverClass()
-  }
-}
-
-function endGame(draw) {
-  if (draw) {
-    messageText.innerText = 'Draw!'
-  } else {
-    messageText
+    swapTurns();
+    setBoardHoverClass
