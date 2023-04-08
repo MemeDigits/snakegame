46866bb98d73b94fb6c0ffaa8a4ba31e51e027b6
MemeDigits <130191257+MemeDigits@users.noreply.github.com>
2023-04-07 22:47:37 -0400

    Update and rename tic-tac-toe.js to script.js

---
+++ script.js
@@ -0,0 +1,50 @@
+const X_CLASS = '69'
+const O_CLASS = '420'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.querySelector('.board')
+const messageText = document.querySelector('.message')
+let oTurn
+
+startGame()
+
+function startGame() {
+  oTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(O_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  messageText.innerText = ''
+}
+
+function handleClick(e) {
+  const cell = e.target
+  const currentClass = oTurn ? O_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+function endGame(draw) {
+  if (draw) {
+    messageText.innerText = 'Draw!'
+  } else {
+    messageText
--- tic-tac-toe.js
+++
@@ -1,6 +0,0 @@
-const gameboard = document.getElementById('gameboard');
-const cells = document.querySelectorAll('.cell');
-const winnerMessage = document.getElementById('winner');
-const resetButton = document.getElementById('reset-btn');
-let is69 = false; // true: 69, false: 420
-let gameboardState = [null, null, null, null, null, null, null,
